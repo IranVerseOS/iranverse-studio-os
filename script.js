9d85af43b6dd1cd9624f7d8c319faf32afb3e438
IranVerseOS <iranversestudio@gmail.com>
2026-07-12 16:13:23 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 console.log("IranVerse OS Initialized");
 console.log(IranVerseHeroes);
-document.body.innerHTML += `
-<hr>
-<h2>System Test</h2>
-<p>Heroes Database Connected</p>
-<p>${IranVerseHeroes[0].name}</p>
-`;
