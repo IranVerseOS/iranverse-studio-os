7c7173b4d0b3d40814f9681b76ffd5bbc6fc9d26
PersiaNexus <iranversestudio@gmail.com>
2026-07-17 22:33:18 +0330

    Create home-engine.js

---
+++ js/home-engine.js
@@ -0,0 +1,25 @@
+class HomeEngine {
+
+    constructor(){
+
+        console.log("IranVerse Home Engine Loaded");
+
+    }
+
+    initialize(){
+
+        console.log("Loading Characters...");
+        console.log("Loading Worlds...");
+        console.log("Loading NFTs...");
+        console.log("Loading Lore...");
+        console.log("Loading AI...");
+        console.log("Loading Quests...");
+        console.log("Loading Events...");
+
+    }
+
+}
+
+const homeEngine = new HomeEngine();
+
+homeEngine.initialize();
